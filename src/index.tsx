e57929e14fa16dd60154869f800ec8158235b35d
ZygmuntBuszyk <zyrgis@gmail.com>
2020-04-04 18:49:42 +0200

    now-ui-kit added

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import "now-ui-kit/assets/css/bootstrap.min.css"
+import "now-ui-kit/assets/scss/now-ui-kit"
+// import "assets/demo/demo.css";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
